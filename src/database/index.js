eb9c83ac3c7b5f216107221568bcfa31af60dd5f
Leandro Lino <leao.lino7@gmail.com>
2022-03-09 11:00:23 -0300

    CoodeshBack :: update :: Procfile

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 //require("dotenv").config({ path: "../.env" });
 require("dotenv").config();
 
-console.log(process.env.DB_CONNECTION);
+console.log(process.env);
 
 mongoose.connect(process.env.DB_CONNECTION, {
   useNewUrlParser: true,
